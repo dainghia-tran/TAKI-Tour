fc318119623dc334181403f9e145bb68de5284a9
unknown <nguyenphuongnam.pascal@gmail.com>
2020-11-24 00:23:21 -0700

    activity_main.xml

--- app/src/main/java/com/aws/takitour/views/RegisterActivity.java
+++ app/src/main/java/com/aws/takitour/views/RegisterActivity.java
@@ -42,7 +42,6 @@ public class RegisterActivity extends AppCompatActivity {
         linkElements();
 
 
-        toolbarReturn.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
         toolbarReturn.setNavigationOnClickListener(v -> {
             finish();
         });
