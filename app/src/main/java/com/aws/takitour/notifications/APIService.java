2053066f5a8bcfffcd7c843d54569ef5e6eb20f3
nhoanglong20 <hoanglongmcs1@gmail.com>
2020-11-26 06:28:05 +0700

    not done noti

---
+++ app/src/main/java/com/aws/takitour/notifications/APIService.java
@@ -0,0 +1,15 @@
+package com.aws.takitour.notifications;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @Headers({
+            "Content-Type:application/json",
+            "Authorization: key=AAAA5IDb_3E:APA91bHGeUbncTMaH5q52TbLEiiF7suxzLQUQtLAZ_ItotF8ya551FWPRih1iI883AG-nSbPTYMgZJQ78CKpEkdpuSfa7HARLPnt_FPiJs2YZMBpqc3XYVizo1EBVnH0Yu4E7YFY6OkU"
+    })
+    @POST("form/send")
+    Call<Response> sendNotification(@Body Sender body);
+}
---
+++ app/src/main/java/com/aws/takitour/notifications/MyTokenService.java
@@ -0,0 +1,6 @@
+package com.aws.takitour.notifications;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyTokenService extends FirebaseMessagingService {
+}
