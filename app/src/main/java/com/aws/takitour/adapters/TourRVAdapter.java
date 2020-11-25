a85e156aeee1a6604c46fc47d8703739e79f8376
unknown <nguyenphuongnam.pascal@gmail.com>
2020-11-26 00:59:08 +0700

    dashboard.xml

--- app/src/main/java/com/aws/takitour/adapters/TourRVAdapter.java
+++ app/src/main/java/com/aws/takitour/adapters/TourRVAdapter.java
@@ -33,58 +33,7 @@ public class TourRVAdapter extends RecyclerView.Adapter<TourRVAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-<<<<<<< HEAD
-        Context context = parent.getContext();
-        LayoutInflater inflater = LayoutInflater.from(context);
 
-        View view = inflater.inflate(R.layout.rv_tour_card,parent,false);
-
-        ViewHolder viewHolder = new ViewHolder(view);
-
-
-
-
-        return viewHolder;
-
-    }
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        Tour tour = tourList.get(position);
-        holder.tourName.setText(tour.getName());
-        holder.rating.setText(String.valueOf(tour.getOverallRating()));
-        holder.cost.setText(tour.getPrice());
-        holder.detail.setText(tour.getDescription());
-        holder.tourGuideName.setText((tour.getTourGuide()));
-
-        holder.tourCard.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                dialog = new Dialog(context);
-                dialog.setContentView(R.layout.dialog_tour_details);
-
-                ImageView imageView = dialog.findViewById(R.id.img_image_detail);
-                TextView tourNameDetail = dialog.findViewById(R.id.tv_tour_name_detail);
-                TextView ratingDetail = dialog.findViewById(R.id.tv_rating_detail);
-                TextView durationDetail = dialog.findViewById(R.id.tv_duration_detail);
-                TextView costDetail = dialog.findViewById(R.id.tv_cost_detail);
-                TextView shortDescriptionDetail = dialog.findViewById(R.id.tv_short_description_detail);
-                TextView tourGuideNameDetail = dialog.findViewById(R.id.tv_tour_guide_name_detail);
-                TextView introduction = dialog.findViewById(R.id.tv_introduction);
-
-                Tour tour = tourList.get(holder.getAdapterPosition());
-
-                tourNameDetail.setText(tour.getName());
-                ratingDetail.setText(String.valueOf(tour.getOverallRating()));
-                durationDetail.setText(tour.getDescription());
-                costDetail.setText(tour.getPrice());
-                shortDescriptionDetail.setText(tour.getDescription());
-                tourGuideNameDetail.setText((tour.getTourGuide()));
-
-                dialog.show();
-            }
-=======
         View view = LayoutInflater.from(context).inflate(R.layout.rv_tour_card, parent, false);    //inflate layout
         return new ViewHolder(view);
     }
@@ -104,7 +53,6 @@ public class TourRVAdapter extends RecyclerView.Adapter<TourRVAdapter.ViewHolder
             Intent tourDashboard = new Intent(context, TourDashboard.class);
             tourDashboard.putExtra("TOUR_ID", tour.getId());
             context.startActivity(tourDashboard);
->>>>>>> e172a3e29952f7759224f4cdfb266736243145e8
         });
     }
 
